6731367feda1ceda032994ae283dfd7747d4e583
ssibkn <ssi.bkn@gmail.com>
2024-11-18 21:01:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Hit Counter
+// Simulate hit counter
 let hitCount = localStorage.getItem('hitCount') || 0;
 hitCount++;
 localStorage.setItem('hitCount', hitCount);
 document.getElementById('hit-count').innerText = hitCount;
 
-// Last Updated
+// Update last updated date dynamically
 document.getElementById('last-updated').innerText = new Date().toLocaleDateString();
